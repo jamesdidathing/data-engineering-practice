1b97892e8b4e5957d733aba58da7632f02879df9
jamesdidathing <jamesalexhodson@gmail.com>
2025-07-23 15:02:11 +0100

    Locate json files, convert into csv

--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -1,8 +1,14 @@
 import boto3
-
+import glob
+import json
+import pandas as pd
 
 def main():
-    # your code here
+    json_files = glob.glob("data/**/*.json", recursive=True)
+    for file in json_files:
+        df = pd.read_json(file)
+        csv = df.to_csv(f"{file}.csv")
+
     pass
 
 
